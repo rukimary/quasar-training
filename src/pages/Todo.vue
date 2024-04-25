b5b50ea1b2ab9f50df90edd7c13322d1bade47f9
mary <rookie.mary@gmail.com>
2024-04-25 19:00:50 +0300

    added todo input functionality

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,19 @@
 <template>
-  <q-page class="bg-brown-1 column"
-    ><h2 class="heading">Todo!</h2>
+  <q-page class="bg-brown-1 column">
+    <div class="todo-wrapper">
+      <h2 class="heading">Todo!</h2>
+      <q-input
+        @keyup.enter="addTask"
+        rounded
+        outlined
+        v-model="newTask"
+        label="Enter a task text"
+      />
+    </div>
 
     <q-list>
       <q-item
+        class="todo-list__item"
         @click="task.done = !task.done"
         clickable
         v-for="(task, index) in tasks"
@@ -38,6 +48,7 @@
 export default {
   data() {
     return {
+      newTask: "",
       tasks: [
         {
           id: 1,
@@ -59,7 +70,28 @@ export default {
   },
   methods: {
     deleteTask(index) {
-      this.tasks.splice(index, 1);
+      this.$q
+        .dialog({
+          dark: true,
+          title: "Confirm",
+          message: "Are you sure?",
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+          this.$q.notify({
+            message: "Successfully deleted",
+            icon: "warning",
+          });
+        });
+    },
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false,
+      });
+      this.newTask = "";
     },
   },
 };
@@ -69,5 +101,16 @@ export default {
 .heading {
   display: flex;
   justify-content: center;
+  margin-bottom: 15px;
+}
+.todo-list__item {
+  min-height: 60px;
+}
+.todo-wrapper {
+  max-width: 600px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
 }
 </style>
